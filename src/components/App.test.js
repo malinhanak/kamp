b271778c75679dfbf6666ea066c45d3e1e2668d4
Malin Hanak <malinehanak@gmail.com>
2021-04-29 15:06:20 +0200

    moved router to wrapper

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,12 +8,7 @@ import { store } from 'store/store';
 
 test('renders learn react link', () => {
 	const history = createMemoryHistory();
-	render(
-		<Router history={history}>
-			<App />
-		</Router>,
-		{ initialState: initialState, store },
-	);
+	render(<App />, { initialState: initialState, store });
 	const linkElement = screen.getByText(/kamp/i);
 	expect(linkElement).toBeInTheDocument();
 });
--- src/config/test-utils.js
+++ src/config/test-utils.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { history } from 'store/store';
+import { Router } from 'react-router';
 
 function render(ui, { initialState, store, ...renderOptions } = {}) {
 	function Wrapper({ children }) {
-		return <Provider store={store}>{children}</Provider>;
+		return (
+			<Provider store={store}>
+				<Router history={history}>{children}</Router>
+			</Provider>
+		);
 	}
 	return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
