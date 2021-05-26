900353822a32a2de178c49263d495c5f4a9f795a
Malin Hanak <malinehanak@gmail.com>
2021-05-26 11:17:36 +0200

    new asset export

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,4 @@ export { default as MainLongBg } from './svg/main-bg-long.svg';
 export { default as MainTest } from './svg/main-bg-test.svg';
 export { default as SvgCircle } from './svg/cricle.svg';
 export { default as GoogleIcon } from './png/GoogleIcon.png';
+export { default as GameItemBG } from './svg/game-item-bg.svg';
