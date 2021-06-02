c7962ef847808e92e04a8d465c4e1f00d3d20307
Malin Hanak <malinehanak@gmail.com>
2021-06-02 08:29:59 +0200

    more height

--- src/styles/ContentWrapper.js
+++ src/styles/ContentWrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.section`
 	width: 100%;
-	height: calc(50% - 200px);
+	height: calc(52% - 200px);
 	margin-top: 150px;
 	display: flex;
 	flex-direction: column;
